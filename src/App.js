7cb90c43d95f45b2f316da62761022c3d259c65c
irriz-jayy <jayfelix789@gmail.com>
2023-03-17 13:38:53 +0300

    Added navbar to correct location

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
-import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage"
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
       <Routes>
         <Route path="/login" element={<Login/>}/>
         <Route path="/" element={<Homepage/>}/>
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,15 @@
+import Navbar from "./Navbar"
+
 const Homepage = () => {
     return (  
+        <>
+                          <Navbar/>
+
         <div className="bg-cyan-800 flex min-h-[100vh] items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
+
             <h2>Yum market</h2>
 </div>
+</>
     );
 }
  
