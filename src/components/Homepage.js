fc6744f537c1b761be1cc9db684eb281d1135378
irriz-jayy <jayfelix789@gmail.com>
2023-03-27 21:39:05 +0300

    Changed homepage landing

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,8 +5,8 @@ const Homepage = () => {
     <>
       <Navbar />
 
-      <div className="bg-cyan-800 flex min-h-[100vh]  py-4 px-4 sm:px-4 lg:px-6 flex-col">
-        <div className="text-center">
+      <div className="bg-cyan-800 flex min-h-[100vh]  flex-col">
+        <div className="h-96  flex justify-center flex-col text-center bg-[url('https://bit.ly/3nrTmqy')] bg-cover">
           <h1 className="font-bold text-4xl">😊Welcome to the Yum Market😊</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
@@ -14,42 +14,10 @@ const Homepage = () => {
             officia sed animi illo assumenda beatae. Nesciunt exercitationem
             excepturi accusantium eos odit consequuntur libero.
           </p>
-        </div>
-        <h2 className="text-2xl text-center font-bold">Categories</h2>
-        <div className="h-96 mt-4 flex flex-row justify-between">
-          <div className="w-1/5 rounded-lg">
-            <img
-              src="https://bit.ly/40vhs1Z"
-              alt="vegetables "
-              className="h-3/4 rounded-lg"
-            />
-            <p className="text-center font-semibold mt-3"> Vegetables </p>
-          </div>
-          <div className="w-1/5 rounded-lg">
-            {" "}
-            <img
-              src="https://bit.ly/3z9kAVv"
-              alt="fruit "
-              className="h-3/4 rounded-lg"
-            />
-            <p className="text-center font-semibold mt-3"> Fruits </p>
-          </div>
-          <div className="w-1/5 rounded-lg">
-            {" "}
-            <img
-              src="https://bit.ly/3JIV1iX"
-              alt="dairy "
-              className="h-3/4 rounded-lg"
-            />
-            <p className="text-center font-semibold mt-3"> Dairy </p>
-          </div>
-          <div className="w-1/5 rounded-lg">
-            <img
-              src="https://bit.ly/42OUrJB"
-              alt="meat "
-              className="h-3/4 rounded-lg"
-            />
-            <p className="text-center font-semibold mt-3"> Meat </p>
+          <div>
+            <button className="rounded-full  w-24 p-2 bg-yellow-400 hover:bg-yellow-500">
+              Shop now
+            </button>
           </div>
         </div>
       </div>
