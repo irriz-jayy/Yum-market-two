f2d6a671d41c612a94e6c1a5d1306a97dfe14753
irriz-jayy <jayfelix789@gmail.com>
2023-03-27 22:14:47 +0300

    Added feature section

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -20,13 +20,61 @@ const Homepage = () => {
             </button>
           </div>
         </div>
-        <div className="h-24 border border-black text-center">
+        <div className="h-24  text-center">
           <p className="font-bold text-2xl p-4">Our features</p>
         </div>
-        <div className="h-96  flex justify-between p-2">
-          <div className=" flex w-3/12 border border-black "></div>
-          <div className=" flex w-3/12 border border-black "></div>
-          <div className=" flex w-3/12 border border-black "></div>
+        <div className="h-80  flex justify-between p-2">
+          <div className=" flex w-3/12 flex-col">
+            <img
+              src="http://via.placeholder.com/350x100"
+              alt="feature"
+              className="h-1/2"
+            />
+            <h2 className="text-center font-black ">Fresh and Organic</h2>
+            <p className="text-center ">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
+              accusantium qui laudantium deserunt debitis dolorem
+            </p>
+            <div>
+              <button className="rounded m-2 p-2 bg-slate-200 hover:bg-slate-400">
+                Read more
+              </button>
+            </div>
+          </div>
+          <div className=" flex w-3/12 flex-col">
+            <img
+              src="http://via.placeholder.com/350x100"
+              alt="feature"
+              className="h-1/2"
+            />
+            <h2 className="text-center font-black ">Quick delivery</h2>
+            <p className="text-center ">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
+              accusantium qui laudantium deserunt debitis dolorem
+            </p>
+            <div>
+              <button className="rounded m-2 p-2 bg-slate-200 hover:bg-slate-400">
+                Read more
+              </button>
+            </div>
+          </div>
+          <div className=" flex w-3/12  flex-col">
+            <img
+              src="http://via.placeholder.com/350x100"
+              alt="feature"
+              className="h-1/2"
+            />
+            <h2 className="text-center font-black ">Easy payments</h2>
+            <p className="text-center ">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
+              accusantium qui laudantium deserunt debitis dolorem
+            </p>
+            <div>
+              <button className="rounded m-2 p-2 bg-slate-200 hover:bg-slate-400">
+                Read more
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
