be93fc8a305fe51c4b349b81eff8a61bb767300b
irriz-jayy <jayfelix789@gmail.com>
2023-03-29 21:17:59 +0300

    Refactored the code

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,52 +1,34 @@
 import "../App.css";
 
+const lists = [
+  {
+    id: 1,
+    image: "http://via.placeholder.com/450x300",
+    name: "Fresh vegetables",
+  },
+  { id: 2, image: "http://via.placeholder.com/450x300", name: "Fresh fruit" },
+  { id: 3, image: "http://via.placeholder.com/450x300", name: "Fresh meat" },
+];
+
 const Categories = () => {
   return (
     <>
       <div className="categories-header">
         <p>Our features</p>
       </div>
-      <div className="categories-container">
-        <div className="category-card">
-          <img
-            src="http://via.placeholder.com/450x300"
-            alt="vegetables "
-            className="category-image"
-          />
-          <div className="category-name">
-            <p className=""> Fresh vegetables </p>
+      <div className="categories-container" >
+        {lists.map((item)=>(
+            <div className="category-card">
+            <img
+              src={item.image}
+              alt="vegetables "
+              className="category-image"
+            />
+            <div className="category-name">
+              <p className=""> {item.name} </p>
+            </div>
           </div>
-        </div>
-        <div className="category-card">
-          <img
-            src="http://via.placeholder.com/450x300"
-            alt="fruit "
-            className="category-image"
-          />
-          <div className="category-name">
-            <p className=""> Fresh fruit</p>
-          </div>
-        </div>
-        <div className="category-card">
-          <img
-            src="http://via.placeholder.com/450x300"
-            alt="dairy "
-            className="category-image"
-          />
-          <div className="category-name">
-            <p className=""> Fresh dairy</p>
-          </div>
-        </div>
-        <div className="category-card">
-          <img
-            src="http://via.placeholder.com/450x300"
-            alt="meat "
-            className="category-image"
-          />
-          <div className="category-name">
-            <p className=""> Fresh meats </p>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
