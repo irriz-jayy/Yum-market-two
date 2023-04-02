c09982c8a45949a30d1c95a58f925f3b3e3a2c68
irriz-jayy <jayfelix789@gmail.com>
2023-04-03 00:11:27 +0300

    Added initial footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,21 @@
+import { FaInstagram, FaTwitter, FaWhatsapp } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <>
       <div className="footer">
-        <h1>Hey</h1>
+        <div className="links">
+          <div className="link-item">
+            <FaInstagram />
+          </div>
+          <div className="link-item">
+            <FaTwitter />
+          </div>
+          <div className="link-item">
+            <FaWhatsapp />
+          </div>
+        </div>
+        <div className="text-lg">Copyright 2023</div>
       </div>
     </>
   );
